a51d78805256c310732566b73dbb1e6a030f4c9c
eke <eke@meelectronic.cn>
2026-01-11 22:58:50 +0800

    开发1.11

--- ruoyi-ui/vue.config.js
+++ ruoyi-ui/vue.config.js
@@ -9,8 +9,8 @@ const CompressionPlugin = require('compression-webpack-plugin')
 
 const name = process.env.VUE_APP_TITLE || '若依管理系统' // 网页标题
 
-const baseUrl = 'http://114.132.93.127:8080' // 后端接口
-
+const baseUrl = 'http://localhost:8081' // 后端接口
+// const baseUrl = 'http://114.132.93.127:8081' // 后端接口
 const port = process.env.port || process.env.npm_config_port || 81 // 端口
 
 // vue.config.js 配置说明
